03da2d61a343537f2ae007c38596f70fe2f70560
ayyush1738 <singhrathoreayush824@gmial.com>
2024-04-22 01:13:07 +0530

    React-App

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,30 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
 
   const [Counter, setCounter] = useState(15);
   // let Counter = 15;
 
-  const addCounter = ()=>{
-    setCounter(Counter + 1);
-  }
+  // const addCounter = ()=>{
+  //   setCounter(Counter + 1);
+  // }
 
-  const removeCount = ()=>{
-    setCounter(Counter - 1);
-  }
+  // const removeCount = ()=>{
+  //   setCounter(Counter - 1);
+  // }
 
   return (
     <>
-      <h1>React App</h1>
-      <h2>Counter Value: {Counter}</h2>
+      <h1 className='bg-green-400 text-white p-4 rounded-xl'>Vite App</h1>
+      {/* <h2>Counter Value: {Counter}</h2>
 
       <button onClick={addCounter}>Add value</button>
       <br />
-      <button onClick={removeCount}>Remove value</button>
+      <button onClick={removeCount}>Remove value</button> */}
+      <Card />
     </>
   )
 }
 
 export default App
+1
